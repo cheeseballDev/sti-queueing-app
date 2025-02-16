ea10dce551cdd03ebd0cae76fa2d065baa6d3587
cheeseballDev <cheeseballbusinessemail@gmail.com>
2025-02-17 00:07:36 +0800

    Added a bunch of colours to be used, STI logo, and fixed the manifest file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.stiqueuingapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.stiqueuingapp"
