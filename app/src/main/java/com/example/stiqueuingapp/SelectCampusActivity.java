e08f0a5f91e2afc5618f303a53cb3455ca939ea7
cheeseball <cheeseballbusinessemail@gmail.com>
2025-02-24 00:08:53 +0800

    Finished the student number verification page's frontend UI.Added necessary icons, strings, and colors.

--- app/src/main/java/com/example/stiqueuingapp/SelectCampusActivity.java
+++ app/src/main/java/com/example/stiqueuingapp/SelectCampusActivity.java
@@ -1,11 +1,6 @@
 package com.example.stiqueuingapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,28 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class SelectCampusActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_select_campus);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        Button newStudentButton = findViewById(R.id.new_student_button);
-        Button oldStudentButton = findViewById(R.id.old_student_button);
-        TextView text = findViewById(R.id.textView);
-        oldStudentButton.setOnClickListener(view -> {
-            startActivity(new Intent(this, OldStudentActivity.class));
-            finish();
-        });
-
-        newStudentButton.setOnClickListener(view -> {
-
         });
     }
 }
