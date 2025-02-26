e7855898122eef55f44a6f130e08a6e738f6d516
Matthew <cheeseballbusinessemail@gmail.com>
2025-02-26 14:13:30 +0800

    Added home activity page

--- app/src/main/java/com/example/stiqueuingapp/HomeActivity.java
+++ app/src/main/java/com/example/stiqueuingapp/HomeActivity.java
@@ -1,8 +1,6 @@
 package com.example.stiqueuingapp;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,32 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SelectCampusActivity extends AppCompatActivity {
-
-    private String[] campuses;
-
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_select_campus);
+        setContentView(R.layout.activity_home);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        initializeDropDown();
-    }
-
-    void initializeDropDown() {
-        Spinner campusDropDown = findViewById(R.id.campus_spinner);
-
-        // TO IMPLEMENT THE FIREBASE DATABASE
-        campuses = new String[]{"Ortigas-Cainta", "Alabang", "Tanay"};
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, campuses);
-        campusDropDown.setAdapter(adapter);
     }
 }
