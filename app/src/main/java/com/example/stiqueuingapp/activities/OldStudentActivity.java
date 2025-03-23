9b419d14fc2e22e04b55a1292d19f19e280dfe6d
cheeseball <cheeseballbusinessemail@gmail.com>
2025-03-23 09:32:43 +0800

    Added firebase logic for multiple fields.

--- app/src/main/java/com/example/stiqueuingapp/activities/OldStudentActivity.java
+++ app/src/main/java/com/example/stiqueuingapp/activities/OldStudentActivity.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -14,21 +16,20 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.stiqueuingapp.R;
 import com.example.stiqueuingapp.activities.models.Student;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-import java.io.FileInputStream;
+import java.util.ArrayList;
 
 public class OldStudentActivity extends AppCompatActivity {
 
     private Button nextPageButton;
     private EditText studentNumberTextField;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private long studentNumber;
+    private ArrayList<Long> studentNumbers = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,13 +39,13 @@ public class OldStudentActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            getStudentNumbers();
             return insets;
-        });
 
+        });
         studentNumberTextField = findViewById(R.id.student_number_number_box);
         nextPageButton = findViewById(R.id.button_verify_page);
 
-        setStudentNumber();
         nextPageButton.setOnClickListener(view -> {
 
             if (studentNumberTextField.getText().toString().trim().equalsIgnoreCase("")) {
@@ -58,30 +59,35 @@ public class OldStudentActivity extends AppCompatActivity {
             }
 
 
-            if (Integer.parseInt(studentNumberTextField.getText().toString()) != studentNumber) {
-                studentNumberTextField.setError("No such student number exists");
-                return;
+            for (int i = 0; i < studentNumbers.size(); i++) {
+                if (Integer.parseInt(studentNumberTextField.getText().toString()) != studentNumbers.get(i)) {
+                    studentNumberTextField.setError("No such student number exists");
+                    return;
+                }
             }
+
 
             startActivity(new Intent(this, HomeActivity.class));
             finish();
         });
     }
 
-    void setStudentNumber() {
 
-        db.collection("STUDENTS").document("STUDENT_ID")
+    void getStudentNumbers() {
+
+        db.collection("STUDENTS")
+                .whereEqualTo("id", true)
                 .get()
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        DocumentSnapshot document = task.getResult();
-                        if (document.exists()) {
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (!task.isSuccessful())
+                            Toast.makeText(OldStudentActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                        for (QueryDocumentSnapshot document : task.getResult()) {
                             Student student = document.toObject(Student.class);
-                            studentNumber = student.getStudentID();
-                        } else {
-                            Log.e("Firestore", "ERROR FETCHING", task.getException());
+                            studentNumbers.add(student.getStudentID());
                         }
                     }
-                    });
+                });
     }
 }
