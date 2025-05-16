707abb939eb337e5e758cbce9fd6986ced503554
cheeseball <cheeseballbusinessemail@gmail.com>
2025-05-16 19:04:29 +0800

    Removed unnecessary lines

--- app/src/main/java/com/example/stiqueuingapp/activities/activities/HomeActivity.java
+++ app/src/main/java/com/example/stiqueuingapp/activities/activities/HomeActivity.java
@@ -492,14 +492,10 @@ public class HomeActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
+            public void onTabUnselected(TabLayout.Tab tab) { }
 
             @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
+            public void onTabReselected(TabLayout.Tab tab) { }
         });
     }
 
